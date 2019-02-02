04e0b20ad14e2b3295a69fc1dcbeda3d339605fb
Edward Boakye <edwardboakye82@Gmail.com>
2019-02-01 22:21:11 -0500

    Commit4

--- src/test/java/tests/LogInTests.java
+++ src/test/java/tests/LogInTests.java
@@ -18,33 +18,44 @@ public class LogInTests {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
-    public void loginTest1(){
+    public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
+
     @Test
-    public void logOutTest(){
+    public void negativeloginTest() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
 
-        driver.findElement(By.id("ctl00_logout")).click();
-
-        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
     }
 
-    @AfterMethod
-    public void cleanUp(){
-        driver.close();
-    }
+        @Test
+        public void logOutTest(){
+            driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+            driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+            driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
 
-}
+            driver.findElement(By.id("ctl00_logout")).click();
+
+            Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+        }
+
+        @AfterMethod
+        public void cleanUp (){
+            driver.close();
+        }
+
+    }
